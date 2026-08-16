804a525c24bef5642f8e15775fe12f10e220dc88
Rohit kumar <1009rohitkumar@gmail.com>
2026-08-16 10:17:26 +0530

    first commit

---
+++ backend/janSetu/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JansetuConfig(AppConfig):
+    name = 'janSetu'
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>SIH 2026</h1>
+      <p>Welcome to our platform.</p>
+    </div>
+  );
+}
+
+export default Home;
