c5c901a64cc01c6da65d73e1b88abc957118fa44
Rack-Rhythm <rackrhythm@gmail.com>
2026-08-17 16:47:21 +0530

    fix: allow all hostnames in ALLOWED_HOSTS for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!)_h3cd0%(0o2-$k9q=$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
